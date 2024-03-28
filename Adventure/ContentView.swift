94497a35781c87068be8affdd5b27974d1297c25
Eklou Amemassovor <amekley@gmail.com>
2024-03-28 08:01:43 +0000

    custom font added

--- Adventure/ContentView.swift
+++ Adventure/ContentView.swift
@@ -10,10 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+            Image(systemName: "slowmo", variableValue: 0.5)
+                .symbolRenderingMode(.palette)
+                .foregroundStyle(.indigo)
+                .font(.largeTitle)
             Text("Hello, world!")
+                .font(.custom("Kanit",size: 20))
         }
         .padding()
     }
