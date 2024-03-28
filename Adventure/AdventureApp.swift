51b26767ad085d017f175687c0fd41429befaa81
Eklou Amemassovor <amekley@gmail.com>
2024-03-28 03:05:35 +0000

    Initial Commit

---
+++ Adventure/AdventureApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdventureApp.swift
+//  Adventure
+//
+//  Created by Siska on 28/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct AdventureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
